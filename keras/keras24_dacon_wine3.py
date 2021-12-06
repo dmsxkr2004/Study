3f9e7dcac0f43d47459e730cee9476d698df2ea8
dmsxkr2004 <94835834+dmsxkr2004@users.noreply.github.com>
2021-12-06 22:59:01 +0900

    Add files via upload

--- keras/keras24_dacon_wine3.py
+++ keras/keras24_dacon_wine3.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import load_wine
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 import numpy as np
 import pandas as pd
 import time
@@ -10,6 +10,7 @@ from sklearn import metrics
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.layers.core import Dropout
+from tensorflow.python.keras.saving.save import load_model
 
 #1. 데이터
 path = "../_data/dacon/wine/"
@@ -30,7 +31,7 @@ le.fit(label)
 test_file['type'] = le.transform(label)
 y = get_dummies(y)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.9, shuffle = True, random_state = 13)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.9, shuffle = True, random_state = 39)
 
 scaler = RobustScaler()
 scaler.fit(x_train)
@@ -40,15 +41,15 @@ test_file = scaler.transform(test_file)
 
 #2. 모델구성
 model = Sequential()
-model.add(Dense(70, input_dim = 12))# linear= 기본값
-model.add(Dense(55))
+model.add(Dense(50, input_dim = 12))# linear= 기본값
+model.add(Dense(35))
 model.add(Dropout(0.5))
-model.add(Dense(40, activation = 'relu'))
-model.add(Dense(25))
-model.add(Dense(10))
+model.add(Dense(20, activation = 'relu'))
+model.add(Dense(8))
 model.add(Dense(5, activation = 'softmax'))
 
 #3. 컴파일, 훈련
+'''
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy']) # categorical_crossentorpy 결과값이[0~3]이상일때 적합한 값 
                                                                                         # metrics 몇개가 맞았는지 결과값을 보기위해 씀 
                                                                                         # # 리스트[]는 다른 값이 더 들어갈수도 있음
@@ -59,7 +60,29 @@ model.fit(x_train, y_train, epochs=1000, batch_size = 10,
 end = time.time()- start
 
 print("걸린시간 : ", round(end, 3), '초')
+'''
+#3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+###########################################################################
+import datetime
+date = datetime.datetime.now()
+datetime = date.strftime("%m%d_%H%M") # month ,day , Hour, minite # 1206_0456
+# print(datetime)
+filepath = './_ModelCheckPoint/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'    # 2500 - 0.3724.hdf5
+model_path = "".join([filepath, 'wine_', datetime, '_', filename])
+                # ./_ModelCheckPoint/1206_0456_2500-0.3724.hdf5
+############################################################################
 
+es = EarlyStopping(monitor= 'val_loss', patience=150, mode = 'auto', verbose=1)#, restore_best_weights = True)
+mcp = ModelCheckpoint(monitor='val_loss',mode='auto', verbose = 1, save_best_only=True, 
+                      filepath = model_path)
+start = time.time()
+hist = model.fit(x_train, y_train, epochs=1000, batch_size = 5, validation_split = 0.1, callbacks = [es,mcp])
+end = time.time()- start
+print("걸린시간 : ", round(end, 3), '초')
+# model = load_model('./_ModelCheckPoint/wine_1206_2248_0060-0.9007.hdf5')
 #4 평가예측
 loss = model.evaluate(x_test,y_test)
 print("loss : ",loss[0])
@@ -74,7 +97,7 @@ print(np.unique(result_recover))    # value_counts = pandas에서만 먹힌다.
 submission['quality'] = result_recover
 
 # print(submission[:10])
-submission.to_csv(path + "jechul5.csv", index = False)
+submission.to_csv(path + "jechul6.csv", index = False)
 print(result_recover)
 '''
 걸린시간 :  169.211 초
