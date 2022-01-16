7d6fe57fc04a7e4b56888b5b1aacf34f00174458
dmsxkr2004 <94835834+dmsxkr2004@users.noreply.github.com>
2022-01-16 15:37:49 +0900

    Add files via upload

---
+++ ml/m05_kfold_2_train_test_split.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split,KFold,cross_val_score
+
+from sklearn.linear_model import Perceptron
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor # 분류
+from sklearn.linear_model import LogisticRegression, LinearRegression # 분류
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
+datasets = load_iris()
+x = datasets.data
+y = datasets.target
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    shuffle=True, random_state = 66, train_size=0.8)
+n_splits = 5
+kfold = KFold(n_splits=n_splits, shuffle = True, random_state=66)
+
+model = SVC()
+
+scores = cross_val_score(model, x, y, cv = kfold)
+print("ACC : ", scores, "\n cross_val_score : ", round(np.mean(scores),4))
