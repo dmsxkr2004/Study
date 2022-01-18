ca15d735db95bbc1c170f51dd3cb3ba4e38537b2
dmsxkr2004 <94835834+dmsxkr2004@users.noreply.github.com>
2022-01-18 20:35:45 +0900

    Add files via upload

---
+++ ml/m07_gridSearch7_boston.py
@@ -0,0 +1,74 @@
+#실습
+
+#모델 : RandomForestClassifier
+from inspect import Parameter
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold,cross_val_score,StratifiedKFold, GridSearchCV
+from sklearn.linear_model import Perceptron
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor # 분류
+from sklearn.linear_model import LogisticRegression, LinearRegression # 분류
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import accuracy_score, r2_score
+datasets = load_boston()
+
+x = datasets.data
+y = datasets.target
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    shuffle=True, random_state = 66, train_size=0.8)
+
+n_splits = 5
+kfold = KFold(n_splits=n_splits, shuffle = True, random_state = 66)
+parameters = [
+    {'n_estimators' : [100, 200],'max_depth' : [6, 8, 10, 12]},
+    {'min_samples_leaf' : [3, 5, 7, 10],'min_samples_split' : [2, 3, 5, 10]},
+    # {'n_jobs' : [-1, 2, 4]}
+]
+
+# 파라미터 조합으로 2개이상 엮을것
+model = GridSearchCV(RandomForestRegressor(), parameters, cv=kfold, verbose=1, # CV = cross validation
+                     refit = True)
+
+#3. 훈련
+import time
+start = time.time()
+model.fit(x_train,y_train)
+end = time.time()
+#4. 평가, 예측
+
+# x_test = x_train # 과적합 상황 보여주기
+# y_test = y_train # train 데이터로 best_estimator_로 예측뒤 점수를 내면
+                   # best_score_ 나온다.
+
+print("최적의 매개변수 : ", model.best_estimator_) # 최적의 매개변수 :  SVC(C=1, kernel='linear')
+print("최적의 파라미터 : ", model.best_params_) # 최적의 파라미터 :  {'C': 1, 'degree': 3, 'kernel': 'linear'}
+
+print("best_score_ : ", model.best_score_) # best_score_ :  0.9916666666666668 # train 값에서 가장 좋은값 (훈련시킨 데이터)
+print("model.score : ", model.score(x_test, y_test)) # model.score :  0.9666666666666667
+
+y_predict = model.predict(x_test)
+print("r2_score : ", r2_score(y_test, y_predict)) # accuacy_score :  0.9666666666666667
+
+y_pred_best = model.best_estimator_.predict(x_test)
+print("최적 튠 r2_score : ",r2_score(y_test,y_pred_best)) # 최적 튠 ACC :  0.9666666666666667
+
+print("걸린시간 : ", end - start)
+'''
+Fitting 5 folds for each of 24 candidates, totalling 120 fits
+최적의 매개변수 :  RandomForestRegressor(max_depth=12)
+최적의 파라미터 :  {'max_depth': 12, 'n_estimators': 100}
+best_score_ :  0.835092408490978
+model.score :  0.9203896078889932
+r2_score :  0.9203896078889932
+최적 튠 r2_score :  0.9203896078889932
+걸린시간 :  17.59367084503174
+'''
+
+'''
+ValueError: Unknown label type: 'continuous' 회귀인지 분류인지 확인해줘야함 Classifier,  Regressor
+'''
---
+++ ml/m07_gridSearch9_kaggle_bike.py
@@ -0,0 +1,92 @@
+from sklearn.linear_model import Perceptron
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor # 분류
+from sklearn.linear_model import LogisticRegression, LinearRegression # 분류
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import accuracy_score
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score, mean_squared_error #mse
+from sklearn.model_selection import train_test_split,KFold,cross_val_score, GridSearchCV
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, LSTM, Conv1D, Flatten
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
+from sklearn.utils import all_estimators
+
+def RMSE(y_test, y_pred):
+    return np.sqrt(mean_squared_error(y_test, y_pred))
+#1. 데이터 
+path = '../_data/kaggle/bike/'
+train = pd.read_csv(path+'train.csv')
+# print(train)      # (10886, 12)
+test_file = pd.read_csv(path+'test.csv')
+# print(test.shape)    # (6493, 9)
+submit_file = pd.read_csv(path+ 'sampleSubmission.csv')
+# print(submit.shape)     # (6493, 2)
+# print(submit_file.columns)
+x = train.drop(['datetime', 'casual','registered','count'], axis=1) # axis=1 컬럼 삭제할 때 필요함
+test_file = test_file.drop(['datetime'], axis=1)
+y = train['count']
+# 로그변환
+y = np.log1p(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+        train_size =0.7, shuffle=True, random_state = 42)
+
+print(x_train.shape)  # (7620, 8)
+print(x_test.shape)  # (3266, 8)
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+test_file = scaler.transform(test_file)
+
+n_splits = 5
+kfold = KFold(n_splits=n_splits, shuffle = True, random_state = 66)
+parameters = [
+    {'n_estimators' : [100, 200, 400],'max_depth' : [6, 8, 10, 12]},
+    {'min_samples_leaf' : [1, 3, 5, 7, 9, 10],'min_samples_split' : [2, 3, 5, 7, 9, 10]},
+    # {'n_jobs' : [-1, 2, 4]}
+]
+
+# 파라미터 조합으로 2개이상 엮을것
+model = GridSearchCV(RandomForestRegressor(), parameters, cv=kfold, verbose=1, # CV = cross validation
+                     refit = True)
+
+#3. 훈련
+import time
+start = time.time()
+model.fit(x_train,y_train)
+end = time.time()
+#4. 평가, 예측
+
+# x_test = x_train # 과적합 상황 보여주기
+# y_test = y_train # train 데이터로 best_estimator_로 예측뒤 점수를 내면
+                   # best_score_ 나온다.
+
+print("최적의 매개변수 : ", model.best_estimator_) # 최적의 매개변수 :  SVC(C=1, kernel='linear')
+print("최적의 파라미터 : ", model.best_params_) # 최적의 파라미터 :  {'C': 1, 'degree': 3, 'kernel': 'linear'}
+
+print("best_score_ : ", model.best_score_) # best_score_ :  0.9916666666666668 # train 값에서 가장 좋은값 (훈련시킨 데이터)
+print("model.score : ", model.score(x_test, y_test)) # model.score :  0.9666666666666667
+
+y_predict = model.predict(x_test)
+print("r2_score : ", r2_score(y_test, y_predict)) # accuacy_score :  0.9666666666666667
+
+y_pred_best = model.best_estimator_.predict(x_test)
+print("최적 튠 r2_score : ",r2_score(y_test,y_pred_best)) # 최적 튠 ACC :  0.9666666666666667
+
+print("걸린시간 : ", end - start)
+'''
+Fitting 5 folds for each of 48 candidates, totalling 240 fits
+최적의 매개변수 :  RandomForestRegressor(max_depth=10, n_estimators=400)
+최적의 파라미터 :  {'max_depth': 10, 'n_estimators': 400}
+best_score_ :  0.34609301435436635
+model.score :  0.3531985313165622
+r2_score :  0.3531985313165622
+최적 튠 r2_score :  0.3531985313165622
+걸린시간 :  181.9608118534088
+'''
